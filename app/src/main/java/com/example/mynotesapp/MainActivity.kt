1888fa283f3a1a4c24bace5389de3c1f57e4a732
abdullah.aqib <abdullah.aqib@mobilelive.ca>
2024-11-27 19:44:32 +0500

    first commit

---
+++ app/src/main/java/com/example/mynotesapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mynotesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.mynotesapp.database.NoteDatabase
+import com.example.mynotesapp.repository.NoteRepository
+import com.example.mynotesapp.viewmodel.NoteViewModel
+import com.example.mynotesapp.viewmodel.NoteViewModelFactory
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
+    }
+}
